2ca0959512c06fdb36b57c0d04a89df877d5a884
Casey Lee <cplee@nektos.com>
2020-05-12 13:46:02 -0700

    latest

--- modules/directory/init.zsh
+++ modules/directory/init.zsh
@@ -10,7 +10,7 @@
 # Options
 #
 
-setopt AUTO_CD              # Auto changes to a directory without typing cd.
+#setopt AUTO_CD              # Auto changes to a directory without typing cd.
 setopt AUTO_PUSHD           # Push the old directory onto the stack on cd.
 setopt PUSHD_IGNORE_DUPS    # Do not store duplicates in the stack.
 setopt PUSHD_SILENT         # Do not print the directory stack after pushd or popd.
